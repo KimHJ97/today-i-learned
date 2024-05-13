f25d49a314ac4a680b7ddd1dfdb57170272bab25
로그 <hj_kim97@naver.com>
2024-05-13 22:05:39 +0900

    가상 스레드 정리

---
+++ BackEnd/Spring/자바 9부터 자바 21까지/03. JDK 18-21/java-from-9-to-21/xxx-api/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+     implementation(project(":xxx-domain"))
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.compileJava {
+    options.compilerArgs.add("--enable-preview")
+}
